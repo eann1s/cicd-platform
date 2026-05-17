4445c703bde9ffd147d8b6921add2cbc42977546
eann1s <danilio.ponomarev7766@gmail.com>
2026-05-17 11:31:29 +0700

    feat(terraform): bootstrap local Kubernetes namespaces

--- infra/terraform/envs/local/main.tf
+++ infra/terraform/envs/local/main.tf
@@ -2,16 +2,31 @@ resource "kubernetes_namespace" "argocd" {
   metadata {
     name = var.argocd_namespace
   }
+  lifecycle {
+    ignore_changes = [
+      metadata[0].annotations
+    ]
+  }
 }
 
 resource "kubernetes_namespace" "go_service" {
   metadata {
     name = var.go_service_namespace
   }
+  lifecycle {
+    ignore_changes = [
+      metadata[0].annotations
+    ]
+  }
 }
 
 resource "kubernetes_namespace" "node_service" {
   metadata {
     name = var.node_service_namespace
+  }
+  lifecycle {
+    ignore_changes = [
+      metadata[0].annotations
+    ]
   }
 }
