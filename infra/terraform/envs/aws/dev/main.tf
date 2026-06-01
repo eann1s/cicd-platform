13576083e794cc2be6b9127c1dd7d04f1ae69a96
eann1s <danilio.ponomarev7766@gmail.com>
2026-06-01 20:46:08 +0300

    feat(terraform): add GitHub OIDC role for ECR pushes

--- infra/terraform/envs/aws/dev/main.tf
+++ infra/terraform/envs/aws/dev/main.tf
@@ -16,3 +16,69 @@ resource "aws_ecr_repository" "go_service" {
   }
   tags = local.common_tags
 }
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  tags            = local.common_tags
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:${var.github_owner}/${var.github_repo}:ref:refs/heads/master",
+        "repo:${var.github_owner}/${var.github_repo}:ref:refs/tags/go-service-v*"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_ecr_push" {
+  name               = "${local.name_prefix}-github-actions-ecr-push"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+  tags               = local.common_tags
+}
+
+data "aws_iam_policy_document" "github_actions_ecr_push" {
+  statement {
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+  statement {
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+      "ecr:DescribeRepositories",
+      "ecr:BatchGetImage"
+    ]
+    resources = [aws_ecr_repository.go_service.arn]
+  }
+}
+
+resource "aws_iam_policy" "github_actions_ecr_push" {
+  name   = "${local.name_prefix}-github-actions-ecr-push"
+  policy = data.aws_iam_policy_document.github_actions_ecr_push.json
+  tags   = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_ecr_push" {
+  role       = aws_iam_role.github_actions_ecr_push.name
+  policy_arn = aws_iam_policy.github_actions_ecr_push.arn
+}
