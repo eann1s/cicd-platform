7b37c7e226624a4304b07f1bfadf8b04f9ff275e
eann1s <danilio.ponomarev7766@gmail.com>
2026-06-04 21:59:08 +0300

    feat(terraform): add remote state backend

--- infra/terraform/envs/aws/dev/providers.tf
+++ infra/terraform/envs/aws/dev/providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket         = "cicd-platform-dev-tfstate-980481493011"
+    key            = "envs/aws/dev/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "cicd-platform-dev-tflock"
+    profile        = "cicd-platform-dev"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
--- infra/terraform/envs/aws/dev/variables.tf
+++ infra/terraform/envs/aws/dev/variables.tf
@@ -32,10 +32,12 @@ variable "vpc_cidr" {
   type    = string
   default = "10.20.0.0/16"
 }
+
 variable "public_subnet_cidrs" {
   type    = list(string)
   default = ["10.20.1.0/24", "10.20.2.0/24"]
 }
+
 variable "availability_zones" {
   type    = list(string)
   default = ["eu-north-1a", "eu-north-1b"]
